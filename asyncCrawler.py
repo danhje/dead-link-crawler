718ae76a028ab8478e6ecd80df559f98a916cc73
Daniel Hjertholm <danhje@gmail.com>
2020-02-03 15:11:03 +0100

    Turned into class

--- asyncCrawler.py
+++ asyncCrawler.py
@@ -51,93 +51,98 @@ class LinkScanner(HTMLParser):
         return urls
 
 
-async def fetch(session, link):
-    assert(isinstance(link, Link))
-    try:
-        async with session.head(link.absoluteTarget, ssl=sslContext) as response:
-            if response.status >= 401:
-                link.works = False
-                return link
-            contentType = response.headers.get('content-type').lower()
-            parsable = any(t in contentType for t in ('text/html', 'text/xml', 'application/xml', 'application/xhtml+xml'))
-            if parsable:
-                async with session.get(link.absoluteTarget, ssl=sslContext) as response:
-                    body = await response.text()
-                    if errorText in body:
-                        link.works = False
-                        return link
-                    else:
-                        link.targetBody = body
-                        link.works = True
-                        return link
-            else:
-                link.works = True
-                return link
-    except Exception:
-        link.works = False
-        return link
+class DeadLinkCrawler:
 
+    def __init__(self):
+        self.checkedLinks = []
+        self._linkSkanner = LinkScanner()
+        self._sslContext = ssl.SSLContext()
+        self._queuedLinks = []
+        self._domain = None
+        self._parsableContentTypes = ('text/html', 'text/xml', 'application/xml', 'application/xhtml+xml')
 
-def linkAlreadyChecked(link):
-    return link.absoluteTarget in (checkedLink.absoluteTarget for checkedLink in checkedLinks)
+    def startCrawl(self, url, maxSimultanousUrlFetches=10, errorText='Not Found'):
+        self._maxSimultanousUrlFetches = maxSimultanousUrlFetches
+        self._errorText = errorText
+        self._domain = '.'.join(urlparse(url).netloc.split('.')[-2:])
+        startLink = Link(relativeTarget=url, linkTitle='Initial URL')  # foundOn?
+        self._queuedLinks.append(startLink)
+        self._loop = asyncio.get_event_loop()
+        self._loop.run_until_complete(self._main())
 
+    def _linkAlreadyChecked(self, link):
+        return link.absoluteTarget in (checkedLink.absoluteTarget for checkedLink in self.checkedLinks)
 
-def linkAlreadyQueued(link):
-    return link.absoluteTarget in (queuedLink.absoluteTarget for queuedLink in queuedLinks)
+    def _linkAlreadyQueued(self, link):
+        return link.absoluteTarget in (queuedLink.absoluteTarget for queuedLink in self._queuedLinks)
 
+    def _linkIsInternal(self, link):
+        linkDomain = '.'.join(urlparse(link.absoluteTarget).netloc.split('.')[-2:])
+        return linkDomain == self._domain
 
-def onSameDomain(firstLink, secondLink):
-    firstDomain = '.'.join(urlparse(firstLink.absoluteTarget).netloc.split('.')[-2:])
-    secondDomain = '.'.join(urlparse(secondLink.absoluteTarget).netloc.split('.')[-2:])
-    return firstDomain == secondDomain
+    async def _fetch(self, session, link):
+        assert(isinstance(link, Link))
+        try:
+            async with session.head(link.absoluteTarget, ssl=self._sslContext) as response:
+                if response.status >= 401:
+                    link.works = False
+                    return link
+                contentType = response.headers.get('content-type').lower()
+                parsable = any(t in contentType for t in self._parsableContentTypes)
+                if parsable:
+                    async with session.get(link.absoluteTarget, ssl=self._sslContext) as response:
+                        body = await response.text()
+                        if self._errorText in body:
+                            link.works = False
+                            return link
+                        else:
+                            link.targetBody = body
+                            link.works = True
+                            return link
+                else:
+                    link.works = True
+                    return link
+        except Exception as e:
+            link.works = False
+            return link
 
+    async def _main(self):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            while True:
+                completedTasks = [task for task in tasks if task.done()]
+                tasks = [task for task in tasks if not task.done()]
 
-async def main():
-    linkSkanner = LinkScanner()
-    startLink = Link(foundOn='http://danielhjertholm.me/prosjekter.htm', relativeTarget='http://danielhjertholm.me/prosjekter.htm')
-    queuedLinks.append(startLink)
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        simultaneousUrlFetches = 10
-        while True:
-            completedTasks = [task for task in tasks if task.done()]
-            tasks = [task for task in tasks if not task.done()]
+                for task in completedTasks:
+                    parentLink = task.result()
+                    if not parentLink.works:
+                        print(f'Dead link with title "{parentLink.linkTitle}" and target {parentLink.absoluteTarget} found on {parentLink.foundOn}')
+                    if parentLink.targetBody:
+                        self._linkSkanner.feed(parentLink.targetBody)
+                        childLinksFound = self._linkSkanner.popUrls()
+                        self._linkSkanner.reset()
+                        for childLink in childLinksFound:
+                            childLink.foundOn = parentLink.absoluteTarget
+                            if not self._linkAlreadyChecked(childLink) and not self._linkAlreadyQueued(childLink) and self._linkIsInternal(childLink):
+                                self._queuedLinks.append(childLink)
 
-            for task in completedTasks:
-                parentLink = task.result()
-                if not parentLink.works:
-                    print(f'Dead link with title "{parentLink.linkTitle}" and target {parentLink.absoluteTarget} found on {parentLink.foundOn}')
-                if parentLink.targetBody:
-                    linkSkanner.feed(parentLink.targetBody)
-                    childLinksFound = linkSkanner.popUrls()
-                    for childLink in childLinksFound:
-                        childLink.foundOn = parentLink.absoluteTarget
-                        if not linkAlreadyChecked(childLink) and not linkAlreadyQueued(childLink) and onSameDomain(parentLink, childLink):
-                            queuedLinks.append(childLink)
+                if len(tasks) < self._maxSimultanousUrlFetches and len(self._queuedLinks) > 0:
+                    for i in range(self._maxSimultanousUrlFetches - len(tasks)):
+                        if len(self._queuedLinks) > 0:
+                            nextLink = self._queuedLinks.pop(0)
+                            self.checkedLinks.append(nextLink)
+                            newtask = asyncio.create_task(self._fetch(session, nextLink))
+                            tasks.append(newtask)
 
-            if len(tasks) < simultaneousUrlFetches and len(queuedLinks) > 0:
-                for i in range(simultaneousUrlFetches - len(tasks)):
-                    if len(queuedLinks) > 0:
-                        nextLink = queuedLinks.pop(0)
-                        checkedLinks.append(nextLink)
-                        newtask = asyncio.create_task(fetch(session, nextLink))
-                        tasks.append(newtask)
-
-            if len(tasks) == 0 and len(queuedLinks) == 0:
-                break
-            await asyncio.sleep(0.01)
+                if len(tasks) == 0 and len(self._queuedLinks) == 0:
+                    break
+                await asyncio.sleep(0.01)
 
 
 if __name__ == '__main__':
-    sslContext = ssl.SSLContext()
-    errorText = 'Not Found'
-    checkedLinks = []
-    queuedLinks = []
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-
-    deadLinks = [link.absoluteTarget for link in checkedLinks if not link.works]
-    print(f'{len(checkedLinks)} URLs checked. Of those, {len(deadLinks)} were dead.')
+    crawler = DeadLinkCrawler()
+    crawler.startCrawl('http://danielhjertholm.me/prosjekter.htm')
+    deadLinks = [link.absoluteTarget for link in crawler.checkedLinks if not link.works]
+    print(f'{len(crawler.checkedLinks)} URLs checked. Of those, {len(deadLinks)} were dead.')
     if len(deadLinks) > 0:
         print('Dead urls:\n' + '\n'.join(deadLinks))
