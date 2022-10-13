a384f5ff010379dbaad20908fe02b488eeb7a14d
Daniel Hjertholm <daniel.hjertholm@statnett.no>
2022-10-13 13:39:46 +0200

    Make main interface a funtion, not class

--- deadLinkCrawler.py
+++ deadLinkCrawler.py
@@ -1,7 +1,10 @@
-from typing import Optional, Generator
+from http.client import BadStatusLine
+from typing import Optional, List
 import asyncio
 import aiohttp
 import ssl
+
+from aiohttp.web_exceptions import HTTPException
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 from time import time
@@ -10,218 +13,207 @@ import uvloop
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
+SSL_CONTEXT = ssl.SSLContext()
+PARSABLE_CONTENT_TYPES = ('text/html', 'text/xml', 'application/xml', 'application/xhtml+xml')
+
 
 class Link:
-    """The Link class represents links.
-
-    Example:
-
-    >>> link = Link('https://domain.com')
-    >>> isinstance(link, Link)
-    True
-    """
+    """The Link class represents links."""
 
     def __init__(self,
-                 relativeTarget: str,
-                 foundOn: Optional[str] = None,
-                 linkTitle: Optional[str] = None,
+                 relative_target: str,
+                 found_on: Optional[str] = None,
+                 link_title: Optional[str] = None,
                  works: Optional[bool] = None,
-                 targetBody: Optional[str] = None) -> None:
-        self.relativeTarget = relativeTarget
-        self.foundOn = foundOn
-        self.linkTitle = linkTitle
+                 target_body: Optional[str] = None) -> None:
+        self.relative_target = relative_target
+        self.found_on = found_on
+        self.link_title = link_title
         self.works = works
-        self.targetBody = targetBody
+        self.target_body = target_body
 
     def __str__(self) -> str:
-        return self.absoluteTarget
+        return self.absolute_target
 
     @property
-    def absoluteTarget(self) -> str:
-        """Return absolute URL for the link.
+    def absolute_target(self) -> str:
+        """Return absolute URL for the link."""
 
-        Example:
-
-        >>> link = Link('about', foundOn='https://domain.com/home')
-        >>> print(link.absoluteTarget)
-        https://domain.com/about
-
-        >>> link = Link('https://otherdomain.com', foundOn='https://domain.com/home')
-        >>> print(link.absoluteTarget)
-        https://otherdomain.com
-        """
-
-        if self.foundOn:
-            return urljoin(self.foundOn, self.relativeTarget)
+        if self.found_on:
+            return urljoin(self.found_on, self.relative_target)
         else:
-            return self.relativeTarget
+            return self.relative_target
 
 
-class DeadLinkCrawler:
-    """Looks for dead (broken) links on a domain.
+def print_results(links: List[Link]) -> None:
+    """Print out a summary of the given list of links."""
 
-    Example:
+    print(f'{len(links)} have been checked.')
 
-    >>> crawler = DeadLinkCrawler()
-    >>> crawler.startCrawl('http://danielhjertholm.me', verbose=False)
-    >>> crawler.printDeadLinks()
-    No dead links have been found.
-    >>> len(crawler.checkedLinks)
-    2
-    >>> len(list(crawler.deadLinks))
-    0
-    """
+    dead_links = [link for link in links if link.works is False]
 
-    def __init__(self) -> None:
-        self.checkedLinks = []
-        self._sslContext = ssl.SSLContext()
-        self._queuedLinks = []
-        self._domain = None
-        self._parsableContentTypes = ('text/html', 'text/xml', 'application/xml', 'application/xhtml+xml')
-
-    def startCrawl(self,
-                   url: str,
-                   maxConcurrentRequests: int = 10,
-                   errorText: str = 'Not Found',
-                   verbose: bool = True) -> None:
-        """Start scanning for dead links on the given URL."""
-
-        self._verbose = verbose
-        self._maxConcurrentRequests = maxConcurrentRequests
-        self._errorText = errorText
-        self._domain = '.'.join(urlparse(url).netloc.split('.')[-2:])
-        startLink = Link(relativeTarget=url, linkTitle='Initial URL')
-        self._queuedLinks.append(startLink)
-        self._loop = asyncio.get_event_loop()
-        self._loop.run_until_complete(self._main())
-
-    def printDeadLinks(self) -> None:
-        """Print out a summary of all dead links that have been found."""
-
-        groupedLinks = {}
-        for link in self.deadLinks:
-            if link.foundOn in groupedLinks:
-                groupedLinks[link.foundOn].append(link)
-            else:
-                groupedLinks[link.foundOn] = [link]
-        if len(groupedLinks) > 0:
-            for foundOn, deadLinks in groupedLinks.items():
-                print(f'On the page {foundOn}, the following links were dead:')
-                for deadLink in deadLinks:
-                    print(f'  Link title: {deadLink.linkTitle}')
-                    print(f'  Link URL: {deadLink.absoluteTarget}')
+    grouped_links = {}
+    for link in dead_links:
+        if link.found_on in grouped_links:
+            grouped_links[link.found_on].append(link)
         else:
-            print('No dead links have been found.')
+            grouped_links[link.found_on] = [link]
+    if grouped_links:
+        for foundOn, dead_links in grouped_links.items():
+            print(f'On the page {foundOn}, the following links were dead:')
+            for dead_link in dead_links:
+                print(f'  Link title: {dead_link.link_title}')
+                print(f'  Link URL: {dead_link.absolute_target}')
+    else:
+        print('No dead links have been found.')
 
-    @property
-    def deadLinks(self) -> Generator:
-        return (link for link in self.checkedLinks if link.works is False)
 
-    def _linkAlreadyChecked(self, link: Link) -> bool:
-        return link.absoluteTarget in (checkedLink.absoluteTarget for checkedLink in self.checkedLinks)
+def _link_in_list(lst: List[Link], link: Link) -> bool:
+    return link.absolute_target in (checked_link.absolute_target for checked_link in lst)
 
-    def _linkAlreadyQueued(self, link: Link) -> bool:
-        return link.absoluteTarget in (queuedLink.absoluteTarget for queuedLink in self._queuedLinks)
 
-    def _linkIsInternal(self, link: Link) -> bool:
-        linkDomain = '.'.join(urlparse(link.absoluteTarget).netloc.split('.')[-2:])
-        return linkDomain == self._domain
+def _link_is_internal(domain, link: Link) -> bool:
+    return domain == '.'.join(urlparse(link.absolute_target).netloc.split('.')[-2:])
 
-    def _allWorkIsDone(self):
-        return len(self._tasks) == 0 and len(self._queuedLinks) == 0
 
-    @staticmethod
-    def _find_links(html: str) -> list:
-        return [
-            Link(relativeTarget=href, linkTitle=(aTag.text or '<untitled>'))
-            for aTag in BeautifulSoup(html, features="html.parser").find_all('a')
-            if (href := aTag.attrs.get('href')) is not None
-        ]
+def _find_links(html: str) -> List[Link]:
+    return [
+        Link(relative_target=href, link_title=(aTag.text or '<untitled>'))
+        for aTag in BeautifulSoup(html, features="html.parser").find_all('a')
+        if (href := aTag.attrs.get('href')) is not None
+    ]
 
-    def _parseAndDiscardCompletedTasks(self):
-        completedTasks = [task for task in self._tasks if task.done()]
-        self._tasks = [task for task in self._tasks if not task.done()]
 
-        for task in completedTasks:
-            parentLink = task.result()
-            if self._verbose and parentLink.works is False:
-                print(f'Dead link with title "{parentLink.linkTitle}" and target {parentLink.absoluteTarget} found on {parentLink.foundOn}')
-            if parentLink.targetBody:
-                childLinksFound = self._find_links(parentLink.targetBody)
-                for childLink in childLinksFound:
-                    childLink.foundOn = parentLink.absoluteTarget
-                    if not self._linkAlreadyChecked(childLink) and not self._linkAlreadyQueued(childLink) and self._linkIsInternal(childLink):
-                        self._queuedLinks.append(childLink)
+def _should_queue_link(link: Link, queued_links: List[Link], checked_links: List[Link], domain: str) -> bool:
+    return (
+        not _link_in_list(checked_links, link) and
+        not _link_in_list(queued_links, link) and
+        _link_is_internal(domain, link)
+    )
 
-    def _startAdditionalTasksFromQueue(self, session: aiohttp.ClientSession):
-        if len(self._tasks) < self._maxConcurrentRequests and len(self._queuedLinks) > 0:
-            for i in range(self._maxConcurrentRequests - len(self._tasks)):
-                if len(self._queuedLinks) > 0:
-                    nextLink = self._queuedLinks.pop(0)
-                    self.checkedLinks.append(nextLink)
-                    newtask = asyncio.create_task(self._fetch(session, nextLink))
-                    self._tasks.append(newtask)
 
-    def _printStatus(self):
-        try:
-            if time() - self._lastStatusPrintoutTime > 10.0:
-                print(f'Status: {len(self.checkedLinks)} links checked. {len([1 for link in self.checkedLinks if link.works is False])} dead.')
-                # Must use "is False" above, otherwise the default value None will evaluate as False for the ~ _maxConcurrentRequests links that are currently being fetched.
-                self._lastStatusPrintoutTime = time()
-        except AttributeError:
-            # If self does not have the attribute _lastStatusPrintoutTime, this is method's first run
-            self._lastStatusPrintoutTime = time()
+def _parse_completed_tasks(tasks, queued_links, checked_links, verbose, domain):
+    completed_tasks = [task for task in tasks if task.done()]
 
-    async def _fetch(self, session: aiohttp.ClientSession, link: Link) -> Link:
-        try:
-            async with session.head(link.absoluteTarget, ssl=self._sslContext) as response:
-                if response.status >= 401:
+    for task in completed_tasks:
+        parent_link = task.result()
+        if verbose and parent_link.works is False:
+            print(f'Dead link with title "{parent_link.link_title}" and target '
+                  f'{parent_link.absolute_target} found on {parent_link.found_on}')
+        if parent_link.target_body:
+            child_links_found = _find_links(parent_link.target_body)
+            for child_link in child_links_found:
+                child_link.found_on = parent_link.absolute_target
+                if _should_queue_link(child_link, queued_links, checked_links, domain):
+                    queued_links.append(child_link)
+
+
+def _start_additional_tasks_from_queue(
+        session: aiohttp.ClientSession,
+        tasks: list,
+        queued_links: List[Link],
+        max_req: int,
+        checked_links: List[Link],
+        error_text: str,
+) -> None:
+    count = min(len(queued_links), max(0, max_req - len(tasks)))
+    for _ in range(count):
+        next_link = queued_links.pop(0)
+        checked_links.append(next_link)
+        new_task = asyncio.create_task(_fetch(session, next_link, error_text))
+        tasks.append(new_task)
+
+
+def _should_print_status(last_status_print_time: float, freq: float = 10.0) -> bool:
+    return time() - last_status_print_time > freq
+
+
+def _print_status(checked_links: list) -> float:
+    n_dead = len([link for link in checked_links if link.works is False])
+    print(f'Status: {len(checked_links)} links checked. {n_dead} dead.')
+    return time()
+
+
+async def _fetch(session: aiohttp.ClientSession, link: Link, error_text: str) -> Link:
+    try:
+        async with session.head(link.absolute_target, ssl=SSL_CONTEXT) as head_response:
+            if head_response.status >= 401:
+                link.works = False
+                return link
+            try:
+                content_type = head_response.headers['content-type'].lower()
+            except AttributeError:  # contentType is None
+                parsable = True
+            else:  # ContentType is not None
+                parsable = any(t in content_type for t in PARSABLE_CONTENT_TYPES)
+
+            if not parsable:
+                link.works = True
+                return link
+
+            async with session.get(link.absolute_target, ssl=SSL_CONTEXT) as response:
+                body = await response.text()
+                if error_text in body:
                     link.works = False
                     return link
+                link.target_body = body
+                link.works = True
+                return link
+    except HTTPException:
+        link.works = False
+        return link
 
-                try:
-                    contentType = response.headers['content-type'].lower()
-                except AttributeError:  # contentType is None
-                    parsable = True
-                else:  # ContentType is not None
-                    parsable = any(t in contentType for t in self._parsableContentTypes)
 
-                if not parsable:
-                    link.works = True
-                    return link
+async def main(
+    url: str,
+    max_req: int = 10,
+    error_text: str = 'Not Found',
+    verbose: bool = True
+) -> List[Link]:
+    start_link = Link(relative_target=url, link_title="<Initial URL>")
+    tasks = []
+    queued_links = [start_link]
+    checked_links = []
+    last_print_status_time = 0.0
+    domain = '.'.join(urlparse(start_link.absolute_target).netloc.split('.')[-2:])
 
-                async with session.get(link.absoluteTarget, ssl=self._sslContext) as response:
-                    body = await response.text()
-                    if self._errorText in body:
-                        link.works = False
-                        return link
-                    link.targetBody = body
-                    link.works = True
-                    return link
-        except Exception:
-            link.works = False
-            return link
+    async with aiohttp.ClientSession() as session:
+        while tasks or queued_links:
+            _parse_completed_tasks(tasks, queued_links, checked_links, verbose, domain)
+            tasks = [task for task in tasks if not task.done()]
+            _start_additional_tasks_from_queue(session, tasks, queued_links, max_req, checked_links, error_text)
+            if verbose and _should_print_status(last_print_status_time):
+                last_print_status_time = _print_status(checked_links)
+            await asyncio.sleep(0.01)
+        return checked_links
 
-    async def _main(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            self._tasks = []
 
-            while not self._allWorkIsDone():
-                self._parseAndDiscardCompletedTasks()
-                self._startAdditionalTasksFromQueue(session)
-                self._printStatus()
-                await asyncio.sleep(0.01)
+def start_crawl(
+    url: str,
+    max_concurrent_requests: int = 10,
+    error_text: str = 'Not Found',
+    verbose: bool = True
+) -> List[Link]:
+    """Starts the crawl.
 
-            if self._verbose:
-                print(f'Crawl finished. Links checked: {len(self.checkedLinks)}. Dead links found: {len([1 for link in self.checkedLinks if link.works is False])}')
+    Args:
+        url: The URL to start the crawl from.
+        max_concurrent_requests: The maximum number of concurrent requests.
+        error_text: The text to look for in the response body to determine if the link is dead.
+            The http status code is also checked.
+        verbose: If True, immediately report broken links found during the crawl.
+
+    Returns:
+        A list of Link objects, with the 'works' attribute set to True or False.
+    """
+
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(main(url, max_concurrent_requests, error_text, verbose))
 
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod(verbose=True)
-
-    crawler = DeadLinkCrawler()
-    crawler.startCrawl('http://danielhjertholm.me/prosjekter.htm', verbose=True)
-    crawler.printDeadLinks()
-    checkedLinks = crawler.checkedLinks
-    deadLinks = list(crawler.deadLinks)
+    start = time()
+    results = start_crawl('http://danielhjertholm.me/prosjekter.htm', verbose=True)
+    print_results(results)
+    end = time()
+    print(f'Total time: {end - start} seconds')
